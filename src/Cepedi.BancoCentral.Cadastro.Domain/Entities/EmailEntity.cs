88b4c855b1e91fa3bdbec727c5902a30f82e4606
albertolunia <albertolunia@gmail.com>
2024-04-11 17:05:01 -0300

    Criando entidades relacionadas a pessoa

---
+++ src/Cepedi.BancoCentral.Cadastro.Domain/Entities/EmailEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Cepedi.BancoCentral.Cadastro.Domain;
+
+public class EmailEntity
+{
+    public int IdEmail { get; set; }
+    public string EnderecoEmail { get; set; } = default!;
+    public int IdPessoa { get; set; }
+}
