9b7520238889a38b52f237771688be08b5420599
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-23 14:57:17 -0300

    Implementação parcial PixRepository

---
+++ src/Cepedi.BancoCentral.Cadastro.Dados/EntityTypeConfiguration/PixEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Cepedi.BancoCentral.Cadastro.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cepedi.BancoCentral.Cadastro.Dados.EntityTypeConfiguration;
+
+public class PixEntityTypeConfiguration
+{
+    public void Configure(EntityTypeBuilder<PixEntity> builder)
+    {
+        builder.ToTable("Pix"); // Nome da tabela no banco de dados
+
+        builder.HasKey(p => p.IdPix); // Define a chave primária
+
+        // Mapeamento das propriedades
+        builder.Property(p => p.IdPix).IsRequired();
+        builder.Property(p => p.ChavePix).IsRequired();
+        builder.Property(p => p.Agencia).IsRequired();
+        builder.Property(p => p.Conta).IsRequired();
+        builder.Property(p => p.Instituicao).IsRequired();
+        builder.Property(p => p.IdTipoPix).IsRequired();
+
+        // Relacionamento com a entidade TipoPix
+        builder.HasOne(p => p.TipoPix)
+               .WithMany()
+               .HasForeignKey(p => p.IdTipoPix);
+    }
+}
---
+++ src/Cepedi.BancoCentral.Cadastro.Dados/Repositories/PixRepository.cs
@@ -0,0 +1,22 @@
+﻿using Cepedi.BancoCentral.Cadastro.Dominio.Entidades;
+using Cepedi.BancoCentral.Cadastro.Dominio.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cepedi.BancoCentral.Cadastro.Dados.Repositories;
+
+public class PixRepository : IPixRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public PixRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PixEntity> CriarPixAsync(PixEntity pix)
+    {
+        
+    }
+
+
+}
