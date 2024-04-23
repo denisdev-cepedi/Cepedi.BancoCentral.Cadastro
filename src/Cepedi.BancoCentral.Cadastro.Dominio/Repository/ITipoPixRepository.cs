660d32cc595a879a2ef03b314d67801332abc92a
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-23 12:36:05 -0300

    Criação dos Repositorios das entidade Pix e Tipo pix

---
+++ src/Cepedi.BancoCentral.Cadastro.Dominio/Repository/ITipoPixRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cepedi.BancoCentral.Cadastro.Dominio.Entidades;
+
+namespace Cepedi.BancoCentral.Cadastro.Dominio.Repository;
+
+public interface ITipoPixRepository
+{
+    Task<TipoPix> CriarPixAsync(TipoPix pix);
+    Task<TipoPix> ObterPixAsync(int id);
+    Task<TipoPix> AtualizarPixAsync(TipoPix pix);
+    Task<TipoPix> DeletarPixAsync(int id);
+    Task<List<TipoPix>> GetPixsAync();
+}
