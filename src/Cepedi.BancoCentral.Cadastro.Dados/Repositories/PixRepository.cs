9b452271ee7b024875bf3960d4e03efe8ebd8db0
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-23 15:53:22 -0300

    Finalização da implementação pix respository

--- src/Cepedi.BancoCentral.Cadastro.Dados/Repositories/PixRepository.cs
+++ src/Cepedi.BancoCentral.Cadastro.Dados/Repositories/PixRepository.cs
@@ -15,7 +15,41 @@ public class PixRepository : IPixRepository
 
     public async Task<PixEntity> CriarPixAsync(PixEntity pix)
     {
-        
+        _context.Pix.Add(pix);
+        await _context.SaveChangesAsync();
+        return pix;
+    }
+
+    public async Task<PixEntity> AtualizarPixAsync(PixEntity pix)
+    {
+        _context.Pix.Update(pix);
+        await _context.SaveChangesAsync();
+        return pix;
+    }
+
+    public async Task<PixEntity> ObterPixAsync(int id)
+    {
+        return await _context.Pix.Where(p => p.IdPix == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<PixEntity> DeletarPixAsync(int id)
+    {
+        var pix = await ObterPixAsync(id);
+        if (pix == null)
+        {
+            return null;
+        }
+
+        _context.Pix.Remove(pix);
+
+        await _context.SaveChangesAsync();
+
+        return pix;
+    }
+
+     public async Task<List<PixEntity>> GetPixsAync()
+    {
+        return await _context.Pix.ToListAsync();
     }
 
 
