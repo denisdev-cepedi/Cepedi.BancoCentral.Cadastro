323b2319ab6e8fdef1b4d62e49a86d18d0244e8b
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-04-24 15:49:50 -0300

    feat: estrutura base das entidades Pessoa e Banco, para prosseguir no desenvolvimento de Registro de Transações

--- src/Cepedi.BancoCentral.Cadastro.Dados/ApplicationDbContext.cs
+++ src/Cepedi.BancoCentral.Cadastro.Dados/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
 using Cepedi.BancoCentral.Cadastro.Dominio.Entidades;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +9,8 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<UsuarioEntity> Usuario { get; set; } = default!;
     public DbSet<TipoRegistroEntity> TipoRegistro {get; set;} = default!;
+    public DbSet<PessoaEntity> Pessoa {get; set;} = default!;
+    public DbSet<BancoEntity> Banco {get; set;} = default!;
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
