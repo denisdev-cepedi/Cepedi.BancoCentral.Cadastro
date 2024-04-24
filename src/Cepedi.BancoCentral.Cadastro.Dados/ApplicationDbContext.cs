b62015a641ddebd4c9909c62df5cb737c9f932c6
BrendomGoncalves <brendom.vi@gmail.com>
2024-04-24 15:30:25 -0300

    Adicionando banco ao dbset

--- src/Cepedi.BancoCentral.Cadastro.Dados/ApplicationDbContext.cs
+++ src/Cepedi.BancoCentral.Cadastro.Dados/ApplicationDbContext.cs
@@ -2,18 +2,17 @@
 using Cepedi.BancoCentral.Cadastro.Dominio.Entidades;
 using Microsoft.EntityFrameworkCore;
 
-namespace Cepedi.BancoCentral.Cadastro.Dados;
+namespace Cepedi.BancoCentral.Cadastro.Data;
 
 [ExcludeFromCodeCoverage]
 public class ApplicationDbContext : DbContext
 {
     public DbSet<UsuarioEntity> Usuario { get; set; } = default!;
     public DbSet<TipoRegistroEntity> TipoRegistro {get; set;} = default!;
+    public DbSet<BancoEntity> Banco { get; set; } = default!;
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
