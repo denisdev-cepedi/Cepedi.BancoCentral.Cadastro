20e0164bf55ae8aa819a578d04cd8ba4ff965740
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-05-19 20:41:31 -0300

    Requests e Reponses TipoPix

---
+++ src/Cepedi.BancoCentral.Cadastro.Compartilhado/Requests/TipoPix/CriarTipoPixRequest.cs
@@ -0,0 +1,10 @@
+﻿using Cepedi.BancoCentral.Cadastro.Compartilhado.Responses;
+using MediatR;
+using OperationResult;
+
+namespace Cepedi.BancoCentral.Cadastro.Compartilhado.Requests;
+
+public class CriarTipoPixRequest : IRequest<Result<CriarTipoPixResponse>>
+{
+    public string TipoPix {get; set;} = default!;
+}
---
+++ src/Cepedi.BancoCentral.Cadastro.Compartilhado/Requests/TipoPix/GetTiposPixRequest.cs
@@ -0,0 +1,10 @@
+﻿using Cepedi.BancoCentral.Cadastro.Compartilhado.Responses;
+using MediatR;
+using OperationResult;
+
+namespace Cepedi.BancoCentral.Cadastro.Compartilhado.Requests;
+
+public class GetTiposPixRequest : IRequest<Result<List<GetTipoPixResponse>>>
+{
+
+}
