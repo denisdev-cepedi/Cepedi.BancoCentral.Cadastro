632f0d301b39512b1effb3dd44c335de430c6f04
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-05-21 12:06:33 -0300

    feat: interfaces do Unit e do Repository

---
+++ src/Cepedi.BancoCentral.Cadastro.Dominio/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Cepedi.BancoCentral.Cadastro.Dominio.Repository;
+
+public interface IRepository<T> where T : class
+{
+    Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    IRepository<T> Repository<T>()
+        where T: class;
+
+}
