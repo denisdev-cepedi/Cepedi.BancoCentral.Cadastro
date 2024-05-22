205f6925d2a0fc998696954f80d0f08f62dab27e
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-05-22 12:14:54 -0300

    feat: Adicionando o Dapper com a consulta de transações a partir de um valor

--- src/Cepedi.BancoCentral.Cadastro.Compartilhado/Responses/ObtemRegistroTransacaoBancoResponse.cs
+++ src/Cepedi.BancoCentral.Cadastro.Compartilhado/Responses/ObtemRegistroTransacaoBancoResponse.cs
@@ -1,3 +1,2 @@
 ﻿namespace Cepedi.BancoCentral.Cadastro.Compartilhado.Responses;
 public record ObtemRegistroTransacaoBancoResponse (int IdTransacao,DateTime DataRegistro, string NomeTipoRegistro, string NomePessoa, string NomeBanco);
-
---
+++ src/Cepedi.BancoCentral.Cadastro.Dados/Repositories/Queries/RegistroTransacaoBancoQueryRepository.cs
@@ -0,0 +1,38 @@
+﻿using Cepedi.BancoCentral.Cadastro.Compartilhado.DTO;
+using Cepedi.BancoCentral.Cadastro.Compartilhado.Responses;
+using Cepedi.BancoCentral.Cadastro.Dominio.Repository.Queries;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Cepedi.BancoCentral.Cadastro.Data.Repositories.Queries;
+
+public class RegistroTransacaoBancoQueryRepository : BaseDapperRepository, IRegistroTransacaoBancoQueryRepository
+{
+    public RegistroTransacaoBancoQueryRepository(IConfiguration configuration) : base(configuration)
+    {
+
+    }
+
+    public async Task<List<RegistroTransacaoPessoaTipoTransacaoDto>> ObterRegistroTransacaoBancoAsync(double saldo)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@Saldo", saldo, System.Data.DbType.Double);
+
+        var sql = @"SELECT 
+                rtb.IdRegistro as IdRegistro,
+                tr.NomeTipo as NomeTipoRegistro,
+                p.Nome as NomePessoa,
+                rtb.Valor as Valor
+                FROM 
+                    RegistroTransacaoBanco rtb
+                JOIN 
+                    TipoRegistro tr ON rtb.IdTipoRegistro = tr.IdTipoRegistro
+                JOIN 
+                    Pessoa p ON rtb.IdPessoa = p.IdPessoa
+                WHERE 
+                    rtb.Valor >= @Saldo";
+
+        return (await ExecuteQueryAsync<RegistroTransacaoPessoaTipoTransacaoDto>(sql, parametros)).ToList();
+
+    }
+}
